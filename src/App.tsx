ca0b99b72c7e7ab9695cf13ab424f1d54008587f
Jirawat Chanasit <jirawat.chn@spumail.net>
2026-05-07 01:39:49 +0700

    chore: remove @radix-ui/themes and old sidebar CSS classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "./pages/home";
-import "@radix-ui/themes/styles.css";
 import "./App.css";
 import { Detail } from "./pages/detail";
 import { Recommendation } from "./pages/recommend";
 import { RandomMenu } from "./pages/random";
 import { History } from "./pages/history";
 import { Nutrition } from "./pages/nutrition";
+import { ChatHistoryProvider } from "./components/useHistory";
 
 function App() {
   const url = import.meta.env.VITE_API_URL;
   return (
     <BrowserRouter basename="/souschef">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail url={url}/>} />
-        <Route path="/recommendation" element={<Recommendation />} />
-        <Route path="/random" element={<RandomMenu url={url}/>} />
-        <Route path="/history" element={<History />} />
-        <Route path="/nutrition" element={<Nutrition/>}/>
-      </Routes>
+      <ChatHistoryProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail url={url}/>} />
+          <Route path="/recommendation" element={<Recommendation />} />
+          <Route path="/random" element={<RandomMenu url={url}/>} />
+          <Route path="/history" element={<History />} />
+          <Route path="/nutrition" element={<Nutrition/>}/>
+        </Routes>
+      </ChatHistoryProvider>
     </BrowserRouter>
   );
 }
